66019e0efb1b61701cde9017302335d8c8f8c824
KiranYBPatil <kiranpatil8660519@gmail.com>
2026-05-26 06:57:29 +0000

    Added Prometheus metrics endpoint

--- bms-backend/src/server.ts
+++ bms-backend/src/server.ts
@@ -1,3 +1,4 @@
+import { register } from './config/metrics';
 import app from "./app";
 import connectDB from "./config/db";
 
@@ -5,11 +6,20 @@ const startServer = async () => {
   try {
     const PORT = process.env.PORT || 9000;
 
+    // Connect MongoDB
     await connectDB();
 
+    // Prometheus Metrics Endpoint
+    app.get('/metrics', async (_req, res) => {
+      res.set('Content-Type', register.contentType);
+      res.end(await register.metrics());
+    });
+
+    // Start Server
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
+
   } catch (error) {
     console.error("❌ Failed to start server:", error);
     process.exit(1);
